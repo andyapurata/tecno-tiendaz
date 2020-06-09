ac95c67b26dd6a05f9107f76fc885a466e9948ca
Jose Carrillo <jose@apurata.com>
2020-06-09 07:45:12 -0500

    Handle constants + urlencode params for Apurata

--- woocommerce-apurata-payment-gateway.php
+++ woocommerce-apurata-payment-gateway.php
@@ -14,24 +14,25 @@
 */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+const APURATA_TEXT_DOMAIN = 'woocommerce-apurata-payment-gateway';
+const APURATA_DOMAIN = 'http://localhost:8000';
 
 // Check if WooCommerce is active
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+
     function init_wc_apurata_payment_gateway() {
         class WC_Apurata_Payment_Gateway extends WC_Payment_Gateway {
-            const TEXT_DOMAIN = 'woocommerce-apurata-payment-gateway';
-            const APURATA_DOMAIN = 'http://localhost:8000';
 
             public function __construct() {
                 $this->id = 'apurata';
 
-                $this->title = __('Cuotas quincenales', self::TEXT_DOMAIN);
+                $this->title = __('Apurata - Cuotas quincenales', APURATA_TEXT_DOMAIN);
                 $this->icon = 'https://static.apurata.com/img/logo-dark.svg';
                 $this->has_fields = FALSE;
 
                 // Shown in the admin panel:
                 $this->method_title = 'Apurata';
-                $this->method_description = __('Evalúa a tus clientes y financia su compra con cuotas quincenales', self::TEXT_DOMAIN);
+                $this->method_description = __('Evalúa a tus clientes y financia su compra con cuotas quincenales', APURATA_TEXT_DOMAIN);
 
                 $this->init_form_fields();
                 $this->init_settings();
@@ -41,59 +42,52 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                 // $this->title = $this->get_option( 'title' );
 
                 add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-                add_action( 'woocommerce_api_on_approved', array( $this, 'on_approved' ) );
             }
 
-            function on_approved($order_id) {
-                $order          = wc_get_order( $order_id );
-                global $woocommerce;
-                $woocommerce->cart->empty_cart();
-            }
 
             function init_form_fields() {
                 $this->form_fields = array(
                     'enabled' => array
                     (
-                        'title' => __('Habilitar', self::TEXT_DOMAIN) . '/' . __('Deshabilitar', self::TEXT_DOMAIN),
+                        'title' => __('Habilitar', APURATA_TEXT_DOMAIN) . '/' . __('Deshabilitar', APURATA_TEXT_DOMAIN),
                         'type' => 'checkbox',
-                        'label' => __('Habilitar Apurata', self::TEXT_DOMAIN),
+                        'label' => __('Habilitar Apurata', APURATA_TEXT_DOMAIN),
                         'default' => 'yes'
                     ),
                     'client_id' => array
                     (
-                        'title' => __('ID de Cliente', self::TEXT_DOMAIN),
+                        'title' => __('ID de Cliente', APURATA_TEXT_DOMAIN),
                         'type' => 'text',
                         'required' => true,
-                        'description' => __('Este ID se obtiene luego de comunicarte con nosotros', self::TEXT_DOMAIN),
+                        'description' => __('Para obtener este ID comunícate con nosotros al correo merchants@apurata.com', APURATA_TEXT_DOMAIN),
                         'default' => ''
                     ),
                 );
             }
 
             function process_payment( $order_id ) {
-                global $woocommerce;
-                $order = new WC_Order( $order_id );
+                $order = wc_get_order( $order_id );
 
-                $redirect_url = self::APURATA_DOMAIN .
+                $redirect_url = APURATA_DOMAIN .
                                  '/pos/crear-orden-y-continuar' .
-                                 '?order_id=' . $order->get_id() .
-                                 '&pos_client_id=' . $this->client_id.
-                                 '&amount=' . $order->get_total() .
-                                 '&url_redir_on_canceled=http://localhost:8080/?page_id=7' .
-                                 '&url_redir_on_rejected=http://localhost:8080/?page_id=7' .
-                                 '&url_redir_on_success=http://localhost:8080/?page_id=7' .
-                                 '&customer_data__email=' . $order->get_billing_email() .
-                                 '&customer_data__phone=' . $order->get_billing_phone() .
-                                 '&customer_data__billing_address_1=' . $order->get_billing_address_1() .
-                                 '&customer_data__billing_address_2=' . $order->get_billing_address_2() .
-                                 '&customer_data__billing_first_name=' . $order->get_billing_first_name() .
-                                 '&customer_data__billing_last_name=' . $order->get_billing_last_name() .
-                                 '&customer_data__billing_city=' . $order->get_billing_city() .
-                                 '&customer_data__shipping_address_1=' . $order->get_shipping_address_1() .
-                                 '&customer_data__shipping_address_2=' . $order->get_shipping_address_2() .
-                                 '&customer_data__shipping_first_name=' . $order->get_shipping_first_name() .
-                                 '&customer_data__shipping_last_name=' . $order->get_shipping_last_name() .
-                                 '&customer_data__shipping_city=' . $order->get_shipping_city() ;
+                                 '?order_id=' . urlencode($order->get_id()) .
+                                 '&pos_client_id=' . urlencode($this->client_id) .
+                                 '&amount=' . urlencode($order->get_total()) .
+                                 '&url_redir_on_canceled=' . urlencode(wc_get_checkout_url()) .
+                                 '&url_redir_on_rejected=' . urlencode(wc_get_checkout_url()) .
+                                 '&url_redir_on_success=' . urlencode($this->get_return_url( $order )) .
+                                 '&customer_data__email=' . urlencode($order->get_billing_email()) .
+                                 '&customer_data__phone=' . urlencode($order->get_billing_phone()) .
+                                 '&customer_data__billing_address_1=' . urlencode($order->get_billing_address_1()) .
+                                 '&customer_data__billing_address_2=' . urlencode($order->get_billing_address_2()) .
+                                 '&customer_data__billing_first_name=' . urlencode($order->get_billing_first_name()) .
+                                 '&customer_data__billing_last_name=' . urlencode($order->get_billing_last_name()) .
+                                 '&customer_data__billing_city=' . urlencode($order->get_billing_city()) .
+                                 '&customer_data__shipping_address_1=' . urlencode($order->get_shipping_address_1()) .
+                                 '&customer_data__shipping_address_2=' . urlencode($order->get_shipping_address_2()) .
+                                 '&customer_data__shipping_first_name=' . urlencode($order->get_shipping_first_name()) .
+                                 '&customer_data__shipping_last_name=' . urlencode($order->get_shipping_last_name()) .
+                                 '&customer_data__shipping_city=' . urlencode($order->get_shipping_city()) ;
 
 
                 // Return thankyou redirect
