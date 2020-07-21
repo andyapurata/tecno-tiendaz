a42e09f151e9b584aa87c82add1a10e43260cd87
Jose Carrillo <jose@apurata.com>
2020-07-21 13:17:44 -0500

    Add useful comment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# You must set up this config in ~/.ssh/config
+# You must set up the following config in ~/.ssh/config:
+# Host woocommerce_demo
+# 	HostName ec2-34-217-90-133.us-west-2.compute.amazonaws.com
+# 	User ubuntu
+# 	IdentityFile ~/.ssh/apurata.pem
+
 ssh woocommerce_demo "(cd woocommerce-apurata-payment-gateway; git pull;)"
 ssh woocommerce_demo "(cd woocommerce-apurata-payment-gateway; ./deploy_scripts/run_on_server.sh;)"
